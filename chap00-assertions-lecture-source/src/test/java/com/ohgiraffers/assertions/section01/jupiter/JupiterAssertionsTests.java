2b31f00f139cb60d4567df45f4da6253bc67beee
jihyeongeun <hyeongeun0924@gmail.com>
2024-01-29 11:50:33 +0900

    1.29 수업내용
    1.29 수업내용 커밋

---
+++ chap00-assertions-lecture-source/src/test/java/com/ohgiraffers/assertions/section01/jupiter/JupiterAssertionsTests.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.assertions.section01.jupiter;
+
+import org.junit.jupiter.api.Test;
+
+public class JupiterAssertionsTests {
+    @Test
+    public void testAssertEquals(){
+
+    }
+}
